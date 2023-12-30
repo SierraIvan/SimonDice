b780e186087ef738a9a040cfc945976056441765
Sierra <ivan.sierra.perez@colegiolitterator.com>
2023-12-30 22:17:43 +0100

    un pequeño avance, pero sigo perdido, feliz navidad

--- proyecto2eva/src/main/main.java
+++ proyecto2eva/src/main/main.java
@@ -1,23 +1,25 @@
 package main;
 
+
 import java.util.Scanner;
 
 public class main {
 
 	
-	enum Colores{Rojo,Verde,Azul,Dorado}
+	enum tColores{Rojo,Verde,Azul,Dorado}
 	
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		 Colores[] n = new Colores[4];
+		 tColores[] SecuenciaColores = new tColores[12];
 		 
-		 n[0] = Colores.Rojo;
-		 n[1] = Colores.Verde;
-		 n[2] = Colores.Azul;
-		 n[3] = Colores.Dorado;
-
+		 tColores R = tColores.Rojo;
+		 tColores V = tColores.Verde;
+		 tColores A = tColores.Azul;
+		 tColores D = tColores.Dorado;
+		 
+		 
 		
 		System.out.println("Welcome to Simon dice");
 		System.out.println("What is you name? ");
@@ -29,8 +31,25 @@ public class main {
 		int aleatorio = 0;
 		aleatorio = (int) (Math.random() * 3);
 
-		System.out.println(n[aleatorio] + " " + n[aleatorio]);
-
+		System.out.println(R + " " + D);
+		Scanner colores = new Scanner(System.in);
+		char color = colores.next().charAt(0);
+		char color2= colores.next().charAt(0);
+		
+		if(color == 'R') {
+			if(color2 == 'D') {
+				System.out.println("Ganaste");
+			}else {
+				System.out.println("Perdiste");
+			}
+		}else {
+			System.out.println("Perdiste");
+		}
+		
+		
+	
+			
+		
 	}
 	
 	
